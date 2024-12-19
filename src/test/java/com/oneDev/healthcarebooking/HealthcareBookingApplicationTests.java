746cd2c2cebcf2cae6e1fc3962ae85344969289f
Ruswanda2020 <wandasukabumi2020@gmail.com>
2024-12-19 12:57:24 +0700

    first commit

---
+++ src/test/java/com/oneDev/healthcarebooking/HealthcareBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneDev.healthcarebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
